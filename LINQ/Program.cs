a6548c9dfb9083f7eb08e7e0b36cf5deaf834225
Jonathan Borja <jborja.one@gmail.com>
2024-11-14 18:47:46 -0700

    done

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videGames = new List<string>
+            {
+            "The Legend of Zelda",
+            "Super Mario Odyssey",
+            "Minecraft",
+            "Animal Crossing",
+            "Dark Souls",
+            "Halo",
+            "Stardew Valley",
+            "Final Fantasy XV",
+            "Overwatch",
+            "World of Warcraft"
+            };
+
+            var orderedGames = videGames.OrderBy(game => game.Length).ToList();
+
+            Console.WriteLine("List of Video Games ordered by length");
+
+            foreach (var game in orderedGames)
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
